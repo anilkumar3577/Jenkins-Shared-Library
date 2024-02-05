2ba009916181d6ab0c400bccee3655cca5da1bed
anilkumar3577 <107928396+anilkumar3577@users.noreply.github.com>
2024-02-05 17:59:30 +0530

    Update mvnbuild.groovy

--- vars/mvnbuild.groovy
+++ vars/mvnbuild.groovy
@@ -1,3 +1,22 @@
 def call () {
-  sh 'mvn test'
+ pipeline {
+    agent any
+    tools {
+        git 'Default'
+        dockerTool 'docker'
+        maven 'mvn'
+    }
+    stages{
+        stage(code){
+            steps {
+               git 'https://github.com/anilkumar3577/JavaCalculator.git' 
+            }
+        }
+        stage(package){
+            steps {
+               sh 'mvn package'
+            }
+        }
+    }
+ }
 }
